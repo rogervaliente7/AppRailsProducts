ac79c59c1dfa573db625e11f0e783264515e02f1
Rogelio Valiente <rogervalriveraj@gmail.com>
2023-12-31 16:25:13 -0600

    Adding categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories, except: :show
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #product resida en el home, en la raiz
   resources :products, path: '/'
