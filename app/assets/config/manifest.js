71020fc22d952c7c216cbfebf249051223b968e2
Rogelio Valiente <rogervalriveraj@gmail.com>
2024-01-12 17:37:58 -0600

    Adding template of bootstrap in the app

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,9 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+
+//= link jquery.min.js
+//= link jquery_ujs.js
+//= link popper.js
+//= link bootstrap.min.js
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "packs/bootstrap"
+//import "packs/bootstrap"
 
 Rails.start()
 Turbolinks.start()
