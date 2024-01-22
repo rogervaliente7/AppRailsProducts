6144b16bacec6b7d4bd68d6b2a673f7c23d37334
Rogelio Valiente <rogervalriveraj@gmail.com>
2024-01-22 16:28:41 -0600

    Adding login de usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   #users_new 
   namespace :authentication, path: '', as: '' do
     resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create]
   end  
 end
