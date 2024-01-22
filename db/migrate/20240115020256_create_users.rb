87f95ee2ae41de45960e036eb671464ecf5d754b
Rogelio Valiente <rogervalriveraj@gmail.com>
2024-01-22 15:59:35 -0600

    registro de usuarios, hay q cambiar el nombre de la columna diggest cada vez q se haga una migracion por digest

--- db/migrate/20240115020256_create_users.rb
+++ db/migrate/20240115020256_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :email, null: false
       t.string :username, null: false
-      t.string :password_diggest, null: false
+      t.string :password_digest, null: false
 
       t.timestamps
     end
